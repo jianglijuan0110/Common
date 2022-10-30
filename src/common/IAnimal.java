941924c7396cf679a876574f4b91ce554585c2b6
maguish <maguette.sarr.31@gmail.com>
2022-10-30 09:39:49 +0100

    new

---
+++ src/common/IAnimal.java
@@ -0,0 +1,35 @@
+package common;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+
+
+public interface IAnimal extends Remote {
+	
+	//SIGNATURE DES METHODES QUI SERONT IMPLEMENTEES
+	
+	String getNomAnimal() throws RemoteException;
+	void  setNomAnimal(String nomAnimal) throws RemoteException;
+	
+	
+	String getNomMaitre() throws RemoteException;
+	void setNomMaitre(String nomMaitre) throws RemoteException;
+	
+	
+	int getAgeAnimal() throws RemoteException;
+	void  setAgeAnimal(int ageAnimal) throws RemoteException;
+	
+	
+	String getNomComplet() throws RemoteException;
+	
+	
+	String getDossier() throws RemoteException;
+    void setDossier(String suivi) throws RemoteException;
+    
+    
+    String getEspece()throws RemoteException;
+    String nomEspece() throws RemoteException;
+    int dureeVieEspece() throws RemoteException;
+
+}
